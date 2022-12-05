49dba9acdbb35d6ad84d089b746513ef54bc1cb9
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-05 14:55:31 +0100

    I/O file

---
+++ IOstring/InputOutput.cpp
@@ -0,0 +1,26 @@
+//
+// Created by vincenzo on 05/12/22.
+//
+
+//per gestire input e output su file usare #include <fstream>
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    //OFSTREAM crea un riferimento ad un file su cui scrivere
+    //stiamo dichiarando "file1.txt" un oggetto della classe ofstream
+    ofstream filewrite("file1.txt");    //il parametro viene passato al costruttore
+
+    //all'apertura del file, il puntatore memorizza la posizione corrente all'interno di un file
+    //all'apertura è all'inizio del file
+    filewrite << "Ciao Mondo!!" << endl;
+    //.close() chiude il file
+    filewrite.close();
+
+    //file non piu accessibile
+
+    return 0;
+}
