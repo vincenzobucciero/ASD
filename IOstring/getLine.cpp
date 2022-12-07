7e48954377d8ce0cd197db045ec745f53d78a54a
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-07 09:26:24 +0100

    agg

---
+++ IOstring/getLine.cpp
@@ -0,0 +1,29 @@
+//
+// Created by vincenzo on 05/12/22.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    char line[80];
+    fstream ioFile;
+
+    ioFile.open("file1.txt", ios::in | ios::out);
+    //controllo se l'apertura del file è avvenuta con successo
+    if(ioFile.fail())
+        cout<< "Il file non è stato aperto con successo " <<endl;
+    ioFile << "Ciao Gente!";
+    ioFile.seekg(ios::beg);
+    //metodo getline consente la lettura per linea. Legge dalla posizione coorente al piu n caratteri
+    //fermandosi al primo carattere EOF, endline o delimitatore delim
+    ioFile.getline(line, 80);
+    cout << line << endl;
+
+    ioFile.close();
+
+    return 0;
+}
