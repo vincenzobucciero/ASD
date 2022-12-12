8f7d950a91a2092f3821b040b2534aeb02715766
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-12 16:35:40 +0100

    comm List.cpp

---
+++ ExList/List.cpp
@@ -0,0 +1,24 @@
+//
+// Created by vincenzo on 12/12/22.
+//
+
+/*
+
+#include <list>
+#include <cstdlib> // per rand()
+#include <iostream>
+using namespace std;
+int main() {
+    list <int> container;
+    int val;
+    for (int i=0; i<10; i++) {
+        val = (int)((float)rand()/RAND_MAX*10);
+        container.push_back(val);
+    }
+    list <int>::iterator it1;
+    for ( it1=container.begin(); it1!= container.end(); it1++)
+        cout << "elemento della lista: " << *it1 << endl;
+    return 0;
+}
+
+ */
