bd154f1c089fb8e7b6a00af18f94541f2835da99
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-12 17:08:35 +0100

    ex map amap

--- ExMap/Map.cpp
+++ ExMap/Map.cpp
@@ -1,3 +1,38 @@
 //
 // Created by vincenzo on 12/12/22.
 //
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main () {
+    map<string, int> amap;
+    amap["Primo"] = 1;
+    amap["Secondo"] = 2;
+    cout << amap.size() << endl;
+    amap["Terzo"] = 3;
+    amap["Quarto"] = 4;
+    //metodo per inserire
+    amap.insert(pair<string, int>(string("Quinto"), 5));
+    //metodo per inserire
+    amap.insert(make_pair("Sesto", 6));
+    cout << amap.size() << endl;
+
+    map<string, int>::iterator it;
+    for (it = amap.begin(); it != amap.end() ; ++it) {
+        cout << "map:  " << it->first << " " << it->second << endl;
+    }
+
+
+    //Metodo di ricerca che restituisce il valore passando alla find la chiave
+    cout << amap.find("Terzo")->second << endl;
+    //Metodo di ricerca che restituisce il valore
+    cout << amap.at("Quinto") << endl;
+
+    amap.clear();
+
+    return 0;
+}
