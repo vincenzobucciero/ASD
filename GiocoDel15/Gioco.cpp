97c547c2f5d2e513f76c07c3e9f892a08db7adfe
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-14 11:32:01 +0100

    da controllare

---
+++ GiocoDel15/Gioco.cpp
@@ -0,0 +1,44 @@
+//
+// Created by vincenzo on 12/12/22.
+//
+
+/*
+ *  Il gioco del 15 consiste nel disporre le celle da 1 a 15 per riga in una matrice
+    4x4. Avendo a disposizione una sola cella vuota, è possibile spostare solo le
+    celle adiacianti alla cella vuota, in orizzontale ed in verticale.
+
+                    13  10  11  6
+                    5   7   4   8
+                    1   12  14  9
+                    3   15  2
+
+    Progettare ed implementare un programma che legga da file una
+    configurazione del gioco e verifichi se rappresenta la soluzione, usando un
+    contenitore della STL.
+    Implementare una versione parallella che, usando i thread, consenta di
+    verificare più file contemporaneamente.
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "Checker.h"
+
+using namespace std;
+
+int main () {
+    Checker checker1("fileOne.txt");
+    Checker checker2("fileTwo.txt");
+
+    checker1.Check();
+    checker1.getSoluzione();
+    checker1.getInfo();
+
+    return 0;
+}
+
+
+
+
+
